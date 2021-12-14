d6cadf03b76ec91fdfddce4f9157e494abfdabc4
HopeFullee <96109009+HopeFullee@users.noreply.github.com>
2021-12-14 17:28:02 +0900

    Add files via upload
    data for React shop

---
+++ data2.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+
+[
+    {
+        id : 0,
+        title : "Angular",
+        maker : "Google",
+        date : "2010.10",
+        class : "img-angular",
+        detailClass : "detail-angular",
+        img : angularBg,
+        alt : "Angular"
+      },
+    
+      {
+        id : 1,
+        title : "React",
+        maker : "Jordan Walke",
+        date : "2013.05",
+        class : "img-react",
+        detailClass : "detail-react",
+        img : reactBg,
+        alt : "React"
+      },
+    
+      {
+        id : 2,
+        title : "Vue",
+        maker : "Evan You",
+        date : "2014.02",
+        class : "img-vue",
+        detailClass : "detail-vue",
+        img : vueBg,
+        alt : "vue"
+      }
+]

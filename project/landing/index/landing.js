21ac313fb10163d708cd84989d522c5a2c050158
HopeFullee <96109009+HopeFullee@users.noreply.github.com>
2021-12-17 17:45:37 +0900

    Add files via upload

--- project/landing/index/landing.js
+++ project/landing/index/landing.js
@@ -1,24 +1,38 @@
-const splitCon = document.querySelector('.split-container');
-const section = document.querySelectorAll('.split-container > section');
+const menuCon = document.querySelector('.menu-container');
+const menu = document.querySelectorAll('.menu');
+const mTitle = document.querySelectorAll('.menu-title');
 const conCon = document.querySelector('.content-container');
 const content = document.querySelectorAll('.content');
 
 
-splitCon.addEventListener('click', function(e){
-	for(i=0; i<section.length; i++){
-		if(e.target == section[i]){
-			section[i].classList.add('expand');
-			section[i].classList.add('fade-out');
-			doSetTimeout(i);
-			}
+
+menuCon.addEventListener('click', menuHandler);	
+
+function menuHandler(e) {
+	elem = e.target;
+	while(!elem.classList.contains('menu')){
+		elem = elem.parentNode;
+		if(elem.nodeName == "BODY") {
+			elem = null;
+			return;
 		}
-	})
+	}
+	for(i=0; i<menu.length; i++) {
+		mTitle[i].classList.add('hide');
+		if(elem.dataset.value == (i+1)) {
+			mTitle[i].classList.remove('hide');
+			mTitle[i].style.fontSize = "160px"
+			menu[i].classList.add('expand');
+			menu[i].classList.add('fade-out');
+			doSetTimeout(i);
+		}
+	}
+}
 
 function doSetTimeout(i){
 	setTimeout(function(){
-		splitCon.classList.add('hide');
-		conCon.classList.add('show');
-		content[i].classList.add('show');
+		menuCon.classList.add('hide');
+		content[i].classList.add('flex');
 		content[i].classList.add('fade-in');
 	},2400);
 }
